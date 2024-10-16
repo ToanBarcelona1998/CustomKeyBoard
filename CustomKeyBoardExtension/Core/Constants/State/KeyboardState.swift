f63468ede1d3191d89b9f2d12924c6a261e36b35
Nguyen Van Toan <toannv@192.168.68.60>
2024-10-16 16:13:25 +0700

    Add: state

---
+++ CustomKeyBoardExtension/Core/Constants/State/KeyboardState.swift
@@ -0,0 +1,15 @@
+//
+//  KeyboardState.swift
+//  CustomKeyBoardExtension
+//
+//  Created by Nguyen Van Toan on 10/10/24.
+//
+
+import Foundation
+
+enum KeyboardState{
+    case letter
+    case number
+    case symbol
+    case emoij
+}
---
+++ CustomKeyBoardExtension/Core/Constants/State/ShiftState.swift
@@ -0,0 +1,14 @@
+//
+//  ShiftButtonState.swift
+//  CustomKeyBoardExtension
+//
+//  Created by Nguyen Van Toan on 10/10/24.
+//
+
+import Foundation
+
+enum ShiftState {
+    case normalize
+    case shift
+    case cap
+}
